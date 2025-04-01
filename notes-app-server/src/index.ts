9842577db494c70779d93750213128c1c1fdad29
NicoleWij <nicole.wijkman@gmail.com>
2025-04-01 23:55:22 +0200

    Update
    Set up a DB, set up Prisma and Note model, add notes to API response, Create note endpoint

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -1,15 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express"
 import cors from "cors"
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json())
 app.use(cors())
 
 app.get("/api/notes", async (req, res) => {
-    res.json({ message: "success!" })
+
+    const notes = await prisma.note.findMany();
+
+    res.json({notes})
 })
 
 app.listen(5000, () => {
     console.log("server running on localhost:5000")
+});
+
+app.post("/api/notes", async (req, res) => {
+    const { title, content } = req.body;
+
+    try {
+        const newNote = await prisma.note.create({
+            data: { title, content }
+        });
+        res.status(201).json(newNote);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to create note" });
+    }
 });
